c3bf2dee103000ae90e3521eeb0d14bf76d3029f
guillemrima <rivasmartorellguillem@gmail.com>
2022-11-17 20:00:27 +0100

    first commit

---
+++ js/index-script.js
@@ -0,0 +1,8 @@
+function scroll_left(){
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(400,0)
+}
+function scroll_right(){
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(-400,0)
+}
