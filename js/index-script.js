d612a37348d30d1221c151d151e226038f9b4057
guillemrima <rivasmartorellguillem@gmail.com>
2022-12-02 09:31:01 +0100

    responsive

--- js/index-script.js
+++ js/index-script.js
@@ -1,8 +1,10 @@
-function scroll_left(){
-    var left = document.querySelector(".scroll-images");
-    left.scrollBy(400,0)
-}
-function scroll_right(){
-    var left = document.querySelector(".scroll-images");
-    left.scrollBy(-400,0)
+
+
+    function scroll_left(){
+        var left = document.querySelector(".scroll-images");
+        left.scrollBy(400,0)
+    }
+    function scroll_right(){
+        var left = document.querySelector(".scroll-images");
+        left.scrollBy(-400,0)
 }
